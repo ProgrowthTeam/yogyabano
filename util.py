5b02612d4d6594377a5ef84e9481519f642b4613
ayushprogrowth <160112317+ayushprogrowth@users.noreply.github.com>
2024-05-16 12:20:05 +0530

    Update util.py

--- util.py
+++ util.py
@@ -44,7 +44,7 @@ def find_match(input):
 
 def query_refiner(conversation, query):
 
-    response = client.completions.create(model="gpt-4-turbo",
+    response = client.completions.create(model="gpt-3.5-turbo",
     prompt=f"Given the following user query and conversation log, formulate a question that would be the most relevant to provide the user with an answer from a knowledge base.\n\nCONVERSATION LOG: \n{conversation}\n\nQuery: {query}\n\nRefined Query:",
     temperature=0,
     max_tokens=256,)
